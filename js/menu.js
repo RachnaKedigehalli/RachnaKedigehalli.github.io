418102174b64532c72b0f774861aebfa39fa5ce1
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2020-07-30 11:27:17 +0530

    menu button funtionality added

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 var menu = document.getElementsByClassName('menu-button')[0];
 menu.addEventListener('click', function(){
-    
+    console.log('clicked');
+    document.getElementsByClassName('menu')[0].classList.add('show');
 });
