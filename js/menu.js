f62c47bf5864c821823cd31086775207b2ef712d
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2020-07-30 22:30:43 +0530

    menu dropdown

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 var menu = document.getElementsByClassName('menu-button')[0];
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function(event){
     document.getElementsByClassName('menu')[0].classList.add('show');    
 });
 
 var cross = document.getElementsByClassName('cross')[0];
-cross.addEventListener('click', function(){
+cross.addEventListener('click', function(event){
     document.getElementsByClassName('menu')[0].setAttribute('style', 'animation-name: slide-out;');
     setTimeout(function(){
         document.getElementsByClassName('menu')[0].classList.remove('show');
@@ -12,3 +12,11 @@ cross.addEventListener('click', function(){
     }, 400);
     
 });
+
+var menuDropdown = document.getElementsByClassName('menu-dropdown')[0];
+var dropdownElements = menuDropdown.getElementsByTagName('a');
+for(var i=0; i<dropdownElements.length; i++){
+    dropdownElements[i].addEventListener('click', function(event){
+        cross.click();
+    });
+}
