e9b19021ffcc35e1f3bef70e753b16642972c118
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2020-07-30 12:49:09 +0530

    menu animation added

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,15 @@
 var menu = document.getElementsByClassName('menu-button')[0];
 menu.addEventListener('click', function(){
     console.log('clicked');
-    document.getElementsByClassName('menu')[0].classList.add('show');
+    document.getElementsByClassName('menu')[0].classList.add('show');    
+});
+
+var cross = document.getElementsByClassName('cross')[0];
+cross.addEventListener('click', function(){
+    document.getElementsByClassName('menu')[0].setAttribute('style', 'animation-name: slide-out;');
+    setTimeout(function(){
+        document.getElementsByClassName('menu')[0].classList.remove('show');
+        document.getElementsByClassName('menu')[0].setAttribute('style', 'animation-name: slide-in;');
+    }, 400);
+    
 });
